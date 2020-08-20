6fa712a4ccb412e3d20b24854dc5545ddca8ab33
Pedro Henrique Pereira <pedro23henrique@hotmail.com>
2020-08-20 20:53:50 -0300

    Page Star seccionproduct

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageHeader from '../../components/PageHeader';
 import PageFooter from '../../components/PageFooter';
 
@@ -8,7 +8,20 @@ import Textarea from '../../components/Textarea';
 import './styles.css';
 
 function Contact() {
-    
+    const [name, setName] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [message, setMessage] = useState('');
+
+    function handleSendMessage(e) {
+        e.preventDefault();
+
+        console.log({
+            name,
+            whatsapp,
+            message
+        })
+    };
+
     return (
         <div id="page-contact" className="container">
             <PageHeader title="" />
@@ -18,41 +31,46 @@ function Contact() {
                     <div id="page-contact-data" className="contact-data">
                         <p id="contact-title">Olá, se ainda tiver qualquer dúvida sobre nossos produtos<br /> não hesite em entrar em contato!</p>
                         <br />
-                        <p id="contact-phone">Contato direto via WhatsApp: <a href="https://api.whatsapp.com/send?phone=054996448912&text=Estou entrando em contato através do site, " target="_blank"> (054) 9 9644-8912</a></p>
+                        <p id="contact-phone">WhatsApp: <a href="https://api.whatsapp.com/send?phone=054996448912&text=Estou entrando em contato através do site, " target="_blank"> (054) 9 9644-8912</a></p>
                         <p id="contact-mail">E-mail: <a href="mailto:contact@moveismode.com.br" >contact@moveismode.com.br</a></p>
                         <br />
                         <p id="contact-instagram">Instagram: <a href="https://www.instagram.com/moveismode" target="_blank">@moveismode</a></p>
                         <p id="contact-facebook">Facebook: <a href="https://www.facebook.com/moveismode" target="_blank">@moveismode</a></p>
                         <br />
-                        <iframe 
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d223103.5763828234!2d-51.67571932656326!3d-29.10587677411645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951c234b0cb91f61%3A0x4b97c55bc72ac8da!2sBento%20Gon%C3%A7alves%20-%20RS!5e0!3m2!1sen!2sbr!4v1597865323142!5m2!1sen!2sbr" 
-                        width="100%" 
-                        height="300" 
-                        frameborder="0" 
-                        aria-hidden="false"/>
-                       
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d223103.5763828234!2d-51.67571932656326!3d-29.10587677411645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951c234b0cb91f61%3A0x4b97c55bc72ac8da!2sBento%20Gon%C3%A7alves%20-%20RS!5e0!3m2!1sen!2sbr!4v1597865323142!5m2!1sen!2sbr"
+                            width="100%"
+                            height="300"
+                            frameborder="0"
+                            aria-hidden="false" />
+
                     </div>
                     <div id="page-contact-form" className="page-contact-form">
-                        <form>
+                        <form onSubmit={handleSendMessage}>
                             <fieldset>
                                 <legend>contate-nos</legend>
                                 <Input
+                                    required
                                     name="name"
                                     label="Nome completo"
-                                //value={name}
-                                //onChange={(e) => { setName(e.target.value) }} 
+                                    value={name}
+                                    onChange={(e) => { setName(e.target.value) }}
                                 />
 
                                 <Input
+                                    required
                                     name="whatsapp"
                                     label="WhatsApp"
-                                //onChange={(e) => { setWhatsapp(e.target.value) }} 
+                                    value={whatsapp}
+                                    onChange={(e) => { setWhatsapp(e.target.value) }}
                                 />
 
                                 <Textarea
+                                    required
                                     name="mensagem"
                                     label="Mensagem"
-                                //onChange={(e) => { setBio(e.target.value) }} 
+                                    value={message}
+                                    onChange={(e) => { setMessage(e.target.value) }}
                                 />
                             </fieldset>
                             <footer>
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import PageHeader from '../../components/PageHeader';
 import PageFooter from '../../components/PageFooter';
 
+import Image from '../../assets/images/aparador.jpg';
+
 import './styles.css';
 
 function Inicio() {
@@ -18,12 +20,28 @@ function Inicio() {
 
     }
     return (
-        <div id="page-teacher-list" className="container">
-            <PageHeader/>
+        <div id="page-start" className="container">
+            <PageHeader />
             <main>
+                <div id="page-start-content" className="container">
+                    <div className="section">
+                        <div className="section-title">
+                            <legend>Projetos e produtos</legend>
+                        </div>
+                        <div className="section-product">
+                            <img src={Image} alt="product" />
+                            <img src={Image} alt="product" />
+                            <img src={Image} alt="product" />
+                        </div>
+                        <div className="section-ver-mais">
+                            <a href="">Ver mais</a>
+                        </div>
+
+                    </div>
+                </div>
                 <span id="mouse"></span>
             </main>
-            <PageFooter/>
+            <PageFooter />
         </div>
     )
 }
