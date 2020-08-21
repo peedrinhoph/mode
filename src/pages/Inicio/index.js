bd84fe9c6d09b14924792e4986bae4565a54e5f3
Pedro Henrique Pereira <pedro23henrique@hotmail.com>
2020-08-21 15:58:14 -0300

    section projeto

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -29,20 +29,46 @@ function Inicio() {
                             <legend>Projetos e produtos</legend>
                         </div>
                         <div className="section-product">
-                                <figure>
-                                    <img src={Image} alt="product" className="zoom" />
-                                </figure>
-                                <figure>
-                                    <img src={Image} alt="product" className="zoom" />
-                                </figure>
-                                <figure>
-                                    <img src={Image} alt="product" className="zoom" />
-                                </figure>
+                            <figure>
+                                <img src={Image} alt="product" className="zoom" />
+                            </figure>
+                            <figure>
+                                <img src={Image} alt="product" className="zoom" />
+                            </figure>
+                            <figure>
+                                <img src={Image} alt="product" className="zoom" />
+                            </figure>
                         </div>
                         <div className="section-ver-mais">
                             <a href="">Ver mais</a>
                         </div>
+                    </div>
+                    <div className="section">
+                        <div className="section-product">
+                            <div className="section-project">
+                                <div className="section-project-title">
+                                    <img src="https://d1di2lzuh97fh2.cloudfront.net/files/19/195/195g9m.svg?ph=f273c76eb3" alt="asdas" />
+                                    <h3>Do projeto à execução</h3>
+                                </div>
+                                <p>Clique aqui e comece a escrever. Illo inventore veritatis et quasi architecto beatae vitae.</p>
+                            </div>
 
+                            <div className="section-project">
+                                <div className="section-project-title">
+                                    <img src="https://d1di2lzuh97fh2.cloudfront.net/files/0j/0jc/0jc3z2.svg?ph=f273c76eb3" alt="asdas" />
+                                    <h3>Materiais de primeira</h3>
+                                </div>
+                                <p>Clique aqui e comece a escrever. Illo inventore veritatis et quasi architecto beatae vitae.</p>
+                            </div>
+
+                            <div className="section-project">
+                                <div className="section-project-title">
+                                    <img src="https://d1di2lzuh97fh2.cloudfront.net/files/3w/3wu/3wunxe.svg?ph=f273c76eb3" alt="asdas" />
+                                    <h3>Atenção aos detalhes</h3>
+                                </div>
+                                <p>Clique aqui e comece a escrever. Illo inventore veritatis et quasi architecto beatae vitae.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <span id="mouse"></span>
