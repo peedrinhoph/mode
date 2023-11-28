ac23c3c244bd230a5faea29d19e56e7eddb32a94
Pedro Henrique Pereira <pedro23henrique@hotmail.com>
2023-11-28 19:14:39 -0300

    FEAT: Build

---
+++ build/precache-manifest.04acd694c57e1a447d80cafd78838791.js
@@ -0,0 +1,438 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd933fa9e24f3750acc0ccffb184b02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a14c7666725c8ea2f2a",
+    "url": "/static/css/main.3a346a7b.chunk.css"
+  },
+  {
+    "revision": "48c9a8cb331d5325609c",
+    "url": "/static/js/2.7d1a44d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7d1a44d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a14c7666725c8ea2f2a",
+    "url": "/static/js/main.bd30fb68.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "26e51556c36abaca0217b8c8cb48fd64",
+    "url": "/static/media/MDBQ1001.26e51556.jpg"
+  },
+  {
+    "revision": "707b0109c9d865cfa98270aa39aaad4b",
+    "url": "/static/media/MDPUFF1002.707b0109.png"
+  },
+  {
+    "revision": "0266bb4f4072ab4c3428baea88528c90",
+    "url": "/static/media/MDPUFF1003.0266bb4f.jpg"
+  },
+  {
+    "revision": "3bf9c471db1a860a839320fe020fc3db",
+    "url": "/static/media/MDPUFF1003P.3bf9c471.jpg"
+  },
+  {
+    "revision": "06a2dd2ac66c1f128fa3febb21f6b539",
+    "url": "/static/media/MDPUFF1004.06a2dd2a.jpg"
+  },
+  {
+    "revision": "14465cf86c93878f68dc44810c3f3f01",
+    "url": "/static/media/MDPUFF1004P.14465cf8.jpg"
+  },
+  {
+    "revision": "84e44fe301446d5f9d058b332f83e0d4",
+    "url": "/static/media/Mode-Moveis-4.84e44fe3.png"
+  },
+  {
+    "revision": "1acc8bb14f0c9483f38670255358f6d9",
+    "url": "/static/media/acabamento_marmore_imperial.1acc8bb1.jpg"
+  },
+  {
+    "revision": "1b053dba664111623f7238f46aa18e83",
+    "url": "/static/media/acocortenespecial.1b053dba.jpg"
+  },
+  {
+    "revision": "38009a475a30679445ce73f6ebdfb88e",
+    "url": "/static/media/aparador_arco_embuia.38009a47.jpg"
+  },
+  {
+    "revision": "e39d15d3fe34b08dae5d33e4e99f99d4",
+    "url": "/static/media/aparador_arco_mel.e39d15d3.jpg"
+  },
+  {
+    "revision": "09d9ef8fac8c2c68f0f1db529a323dda",
+    "url": "/static/media/aparador_vintage.09d9ef8f.jpg"
+  },
+  {
+    "revision": "4b165a886f589232d9bc14cfa988650a",
+    "url": "/static/media/aparadorvintage.4b165a88.jpg"
+  },
+  {
+    "revision": "24389b26884082fda6638d86baa058fb",
+    "url": "/static/media/back.24389b26.svg"
+  },
+  {
+    "revision": "6cbaacc4ffc8accfa2c2a3bc1e84d680",
+    "url": "/static/media/banqueta.6cbaacc4.jpg"
+  },
+  {
+    "revision": "d0183ffa9c77d0e0cf5ca4d378dd3685",
+    "url": "/static/media/banqueta_bistro1.d0183ffa.jpg"
+  },
+  {
+    "revision": "9f89884289e6ca0bac81744c33ad6fd8",
+    "url": "/static/media/banqueta_bistro2.9f898842.jpg"
+  },
+  {
+    "revision": "7b0b37cccabcd0351fc37de33f306241",
+    "url": "/static/media/banqueta_concha2.7b0b37cc.jpg"
+  },
+  {
+    "revision": "8720357efd4c15614facb098c9dc60d1",
+    "url": "/static/media/bronze_especial.8720357e.jpg"
+  },
+  {
+    "revision": "2c167e79dc03ed1153601839771da6a2",
+    "url": "/static/media/cadeira_concha1.2c167e79.jpg"
+  },
+  {
+    "revision": "d9a1c787e41447c4de835d2ce15428dd",
+    "url": "/static/media/cadeira_concha2.d9a1c787.jpg"
+  },
+  {
+    "revision": "0d8f0a20a64c16960441cdea534771da",
+    "url": "/static/media/cadeira_concha3.0d8f0a20.jpg"
+  },
+  {
+    "revision": "4a966a85861bc80be82f3597b5d876b5",
+    "url": "/static/media/capri_branco.4a966a85.jpg"
+  },
+  {
+    "revision": "c5069f746bf6b2ee1aeb96291c7ff705",
+    "url": "/static/media/carrara.c5069f74.jpg"
+  },
+  {
+    "revision": "5e05e345b256505b47ee8515f65246e3",
+    "url": "/static/media/costelaferro.5e05e345.png"
+  },
+  {
+    "revision": "e0a0b5cb97221dd2c75a845d37e0abee",
+    "url": "/static/media/costelaferro1.e0a0b5cb.png"
+  },
+  {
+    "revision": "9bca7d1deaa335afd54b78b536031023",
+    "url": "/static/media/costelafixa.9bca7d1d.jpeg"
+  },
+  {
+    "revision": "ac4db76f00467b6371c5f47aefda9c34",
+    "url": "/static/media/costelafixa1.ac4db76f.png"
+  },
+  {
+    "revision": "f0eb012697eb51ce29a65209af2e3b39",
+    "url": "/static/media/costelagiratoria.f0eb0126.png"
+  },
+  {
+    "revision": "434ca95d8dc7dfc18a00adb073762d51",
+    "url": "/static/media/costelagiratoria1.434ca95d.png"
+  },
+  {
+    "revision": "31ef1d2325bd1f644f7a09e19093dabb",
+    "url": "/static/media/denver_05_bege.31ef1d23.jpg"
+  },
+  {
+    "revision": "066b96050524eedbef6f04789ebb71b4",
+    "url": "/static/media/denver_preto_06.066b9605.jpg"
+  },
+  {
+    "revision": "40309b5114a472cb5b9e91cdf7f3d4d0",
+    "url": "/static/media/dourado_especial.40309b51.jpg"
+  },
+  {
+    "revision": "959040f59a63ceb5a435c8c4a52f6a49",
+    "url": "/static/media/facebook.959040f5.svg"
+  },
+  {
+    "revision": "5fb4321961ffbb57ae32bac580b6fbd2",
+    "url": "/static/media/image1.5fb43219.png"
+  },
+  {
+    "revision": "140ae1ce538125d883bb37d5548c370d",
+    "url": "/static/media/imbuia_escura.140ae1ce.jpg"
+  },
+  {
+    "revision": "a4f24e77c957e0d3d6fae2c4c326acb7",
+    "url": "/static/media/imbuia_natural.a4f24e77.jpg"
+  },
+  {
+    "revision": "341a67c538d67f9ce92005cf14255dc2",
+    "url": "/static/media/insta.341a67c5.svg"
+  },
+  {
+    "revision": "0113379e3239cc12f0e2ef8cd5212697",
+    "url": "/static/media/joli_01.0113379e.jpg"
+  },
+  {
+    "revision": "60044d93d7387761ac6c0b4e2fc383b2",
+    "url": "/static/media/joli_04a.60044d93.jpg"
+  },
+  {
+    "revision": "d00811f0b66f9860a058b5eb6b3312c4",
+    "url": "/static/media/joli_azul06.d00811f0.jpg"
+  },
+  {
+    "revision": "de283b7e71c949df2fdc5d9c7fbac0d2",
+    "url": "/static/media/joli_cinza03.de283b7e.jpg"
+  },
+  {
+    "revision": "625607cfa5c320cfbd89a95c9aba3d75",
+    "url": "/static/media/joli_preto12.625607cf.jpg"
+  },
+  {
+    "revision": "a958e29eab395a6f593a52368e7461db",
+    "url": "/static/media/joli_rosa18.a958e29e.jpg"
+  },
+  {
+    "revision": "cceac32bdfce3bc30d5182b9f65d7c0d",
+    "url": "/static/media/joli_rose.cceac32b.jpg"
+  },
+  {
+    "revision": "fa0a8c2d8115b80b871c56d5b676c81b",
+    "url": "/static/media/joli_verde15r.fa0a8c2d.jpg"
+  },
+  {
+    "revision": "2788a044b3f80c50e9413cb104d2925b",
+    "url": "/static/media/joli_vermelho11r.2788a044.jpg"
+  },
+  {
+    "revision": "11a2d479ccbceed2b767d90cf440a692",
+    "url": "/static/media/linhocapribege.11a2d479.jpg"
+  },
+  {
+    "revision": "d37bdc49fe892fa6a1d9174072a15438",
+    "url": "/static/media/logo.d37bdc49.jpg"
+  },
+  {
+    "revision": "c66ee6f637138ab9d58d2e17d58c0aa7",
+    "url": "/static/media/mail.c66ee6f6.svg"
+  },
+  {
+    "revision": "99d0b1028b61fd7348682bfc9ea180f6",
+    "url": "/static/media/mel.99d0b102.jpg"
+  },
+  {
+    "revision": "1d8339298c082720aab96d11e98cd3cb",
+    "url": "/static/media/mesa_bistro.1d833929.jpg"
+  },
+  {
+    "revision": "445ff8a7acbabe4ff621fad5223ee6f3",
+    "url": "/static/media/mesa_bistro_conjunto.445ff8a7.jpg"
+  },
+  {
+    "revision": "1ab913b547538a42bcf97d8b09833d39",
+    "url": "/static/media/mesa_canto_dupla1.1ab913b5.jpg"
+  },
+  {
+    "revision": "9cf3f0319004c2d0a99e7930c559c340",
+    "url": "/static/media/mesa_canto_dupla2.9cf3f031.jpg"
+  },
+  {
+    "revision": "6df34c647e308f71e4cd25ed16d4d98c",
+    "url": "/static/media/mesa_canto_quadrada_embuia2.6df34c64.jpg"
+  },
+  {
+    "revision": "13358fa419eac66211af4dad6697f730",
+    "url": "/static/media/mesa_canto_quadrada_mel1.13358fa4.jpg"
+  },
+  {
+    "revision": "c4edba136ec515e8446dbe9d3487ef71",
+    "url": "/static/media/mesa_canto_vintage.c4edba13.jpg"
+  },
+  {
+    "revision": "f9024309f486c548046007f02939cfc1",
+    "url": "/static/media/mesa_centro_alcada_embuia.f9024309.jpg"
+  },
+  {
+    "revision": "dd6877b017317037a3750d7ee9349e53",
+    "url": "/static/media/mesa_centro_alcada_mel.dd6877b0.jpg"
+  },
+  {
+    "revision": "accd61bd2cd60c41a12c2532c90c4efb",
+    "url": "/static/media/mesa_centro_alianca1.accd61bd.jpg"
+  },
+  {
+    "revision": "0ad9f19fca365208ea6fde708148ddf5",
+    "url": "/static/media/mesa_centro_alianca2.0ad9f19f.jpg"
+  },
+  {
+    "revision": "61a8185d3f5cc53112c0a663d80105e3",
+    "url": "/static/media/mesa_centro_dupla1.61a8185d.jpg"
+  },
+  {
+    "revision": "02ccb0db4d895c7f15fab96b77e46a84",
+    "url": "/static/media/mesa_centro_dupla2.02ccb0db.jpg"
+  },
+  {
+    "revision": "2edc27d10204015a4e12e22fe758b51b",
+    "url": "/static/media/mesa_centro_dupla3.2edc27d1.jpg"
+  },
+  {
+    "revision": "b88e529fba308a5b9bdf2c2c6e6709cc",
+    "url": "/static/media/mesa_centro_electra2.b88e529f.jpg"
+  },
+  {
+    "revision": "d07d37fc019734456b911b6995badc51",
+    "url": "/static/media/mesa_centro_pisa1.d07d37fc.jpg"
+  },
+  {
+    "revision": "6f2852e4ed13811ef3aeed7eb6b33bcf",
+    "url": "/static/media/mesa_centro_pisa2.6f2852e4.jpg"
+  },
+  {
+    "revision": "cba5c2d6d08f280a70325c5a06a52ed0",
+    "url": "/static/media/mesa_centro_pisa3.cba5c2d6.jpg"
+  },
+  {
+    "revision": "d1f69df092657354a468eab414f77301",
+    "url": "/static/media/mesa_centro_pisa4.d1f69df0.jpg"
+  },
+  {
+    "revision": "9b1525da3bad6ce91087b02609d260f8",
+    "url": "/static/media/mesa_centro_retangular_embuia1.9b1525da.jpg"
+  },
+  {
+    "revision": "4a43f3f6df262f20e1876c6bade3d4f1",
+    "url": "/static/media/mesa_centro_retangular_embuia2.4a43f3f6.jpg"
+  },
+  {
+    "revision": "c552bfac23a01e7eeaedde3fa199d34c",
+    "url": "/static/media/mesa_centro_retangular_mel1.c552bfac.jpg"
+  },
+  {
+    "revision": "7a79d2d6c5ef1b8f80b97de3e50a271b",
+    "url": "/static/media/mesa_centro_retangular_mel2.7a79d2d6.jpg"
+  },
+  {
+    "revision": "93d4808a8bc381a35b69cbfa592f37a1",
+    "url": "/static/media/mesa_centro_x_embuia.93d4808a.jpg"
+  },
+  {
+    "revision": "8abd7c444a9f7d9ad5a5dece949a4d34",
+    "url": "/static/media/mesa_centro_x_mel.8abd7c44.jpg"
+  },
+  {
+    "revision": "445ff8a7acbabe4ff621fad5223ee6f3",
+    "url": "/static/media/mesabistro.445ff8a7.jpg"
+  },
+  {
+    "revision": "13358fa419eac66211af4dad6697f730",
+    "url": "/static/media/mesacanto1.13358fa4.jpg"
+  },
+  {
+    "revision": "accd61bd2cd60c41a12c2532c90c4efb",
+    "url": "/static/media/mesacentroaro.accd61bd.jpg"
+  },
+  {
+    "revision": "bb8a9cc7fe856fdff87eb1146d0c5085",
+    "url": "/static/media/nero.bb8a9cc7.jpg"
+  },
+  {
+    "revision": "0b5cd5de35d6179bd7e530ca0d1a9158",
+    "url": "/static/media/nogueira.0b5cd5de.jpg"
+  },
+  {
+    "revision": "922f87064e60758f0fee224bd63c82a8",
+    "url": "/static/media/photo1.922f8706.jpg"
+  },
+  {
+    "revision": "8562b051283429a2dc1b638b8048f533",
+    "url": "/static/media/photo10.8562b051.jpg"
+  },
+  {
+    "revision": "f138c661d0649b07f7f92088cd0850c8",
+    "url": "/static/media/photo11.f138c661.jpg"
+  },
+  {
+    "revision": "ae202cd49072b7a5a550f76f7cdf6eec",
+    "url": "/static/media/photo12.ae202cd4.jpg"
+  },
+  {
+    "revision": "2815ed41f29dc58464778815f0ee4ec3",
+    "url": "/static/media/photo13.2815ed41.jpg"
+  },
+  {
+    "revision": "6d2e9e983f59ead3667a7e3846ded1bb",
+    "url": "/static/media/photo2.6d2e9e98.jpg"
+  },
+  {
+    "revision": "3a33c6291b88cc2327d91b7b79459349",
+    "url": "/static/media/photo3.3a33c629.jpg"
+  },
+  {
+    "revision": "d3e7015ba167d5fcbe896f6e71687203",
+    "url": "/static/media/photo4.d3e7015b.jpg"
+  },
+  {
+    "revision": "78976b0d52f06b33052aebb5f57cfb81",
+    "url": "/static/media/photo5.78976b0d.jpg"
+  },
+  {
+    "revision": "07d2aa33b6fa2a6a40c3249d29156ea5",
+    "url": "/static/media/photo6.07d2aa33.jpg"
+  },
+  {
+    "revision": "1b13fa06abe046e29b0ad34f0d38cb80",
+    "url": "/static/media/photo7.1b13fa06.jpg"
+  },
+  {
+    "revision": "1442a8fe26554cd240abf2746df3490d",
+    "url": "/static/media/photo8.1442a8fe.jpg"
+  },
+  {
+    "revision": "184b32d40bcfa8268bdaeb5041aa7863",
+    "url": "/static/media/photo9.184b32d4.jpg"
+  },
+  {
+    "revision": "578907ee1b34be6382c9ca90d4aa79b8",
+    "url": "/static/media/poltrona_julieta1.578907ee.jpg"
+  },
+  {
+    "revision": "1357a7f02942b58468c78203df2e1c9d",
+    "url": "/static/media/poltrona_julieta2.1357a7f0.jpg"
+  },
+  {
+    "revision": "5a740a42a7be0dd3917a378e1ee47420",
+    "url": "/static/media/puff.5a740a42.jpg"
+  },
+  {
+    "revision": "3f683ad99edfcc51e22f9f371b2c381a",
+    "url": "/static/media/puff_estofado_branco.3f683ad9.jpg"
+  },
+  {
+    "revision": "42392437166e7a8fad105d22531f1092",
+    "url": "/static/media/puff_pelucia_preto2.42392437.jpg"
+  },
+  {
+    "revision": "23bcb31f2bd51c5f48b73c107a99fcb3",
+    "url": "/static/media/tecidobanqueta.23bcb31f.jpg"
+  },
+  {
+    "revision": "571bb57d325888f68b4f7045c29e61e7",
+    "url": "/static/media/veludoverde.571bb57d.jpg"
+  },
+  {
+    "revision": "60090f0287572c67499d1f639619658f",
+    "url": "/static/media/whatsapp.60090f02.svg"
+  },
+  {
+    "revision": "30048b57cc5eb22e2ee2b4e00db4d874",
+    "url": "/static/media/whatsappp.30048b57.svg"
+  }
+]);
